0153a6dd319a37a5f903835729d223a40a6ac069
Sergey <metrolog_omsk@mail.ru>
2022-06-05 14:29:42 +0500

    Решили задачу №5

---
+++ Task5/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа
+//456 - 6
+
+Console.WriteLine("Введите число:");
+int n = Convert.ToInt32(Console.ReadLine());
+int result;
+
+if (n > 99 && n < 1000)
+{
+    result = n % 10;
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("Введено не правильное число");
+}
