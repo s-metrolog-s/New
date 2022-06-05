9de22684e2b56b4325ce49728c421ff605f3eb27
Sergey <metrolog_omsk@mail.ru>
2022-06-05 14:05:17 +0500

    Решил 4 задачу

---
+++ Task4/Program.cs
@@ -0,0 +1,12 @@
+﻿// Все целые числа от N до -N
+
+Console.WriteLine("Введите число:");
+int number = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+int numberMinus = -Math.Abs(number);
+
+while (numberMinus <= number)
+{
+    Console.Write(numberMinus);
+     Console.Write(":");
+    numberMinus++;
+}
